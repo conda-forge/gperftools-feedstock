831bda5fcc4f56e7bcca498e8f3afeb32b69a7b1
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-08-17 08:42:10 -0700

    Added variable for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $(uname) == Darwin ]; then
+  export CC=clang
+  export CXX=clang++
+  export MACOSX_DEPLOYMENT_TARGET="10.9"
+fi
+
 ./autogen.sh
 ./configure  --prefix $PREFIX --enable-libunwind
 
