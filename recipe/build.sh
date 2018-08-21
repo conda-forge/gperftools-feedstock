5b1491d086aa8af381a666625aee3a69d888aee2
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-08-21 12:49:59 -0700

    Remove make check for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,18 +2,18 @@
 
 if [ $(uname) == Darwin ]; then
   export MACOSX_DEPLOYMENT_TARGET="10.9"
-  # ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version == :sierra
   export CFLAGS="$CFLAGS -D_XOPEN_SOURCE"
   
   autoreconf -fiv
   ./configure  --prefix $PREFIX \
     CC=clang CXX=clang++ --disable-debugalloc \
     --disable-dependency-tracking
+  make
+  make install
 else
   ./autogen.sh
   ./configure  --prefix $PREFIX --enable-libunwind
+  make
+  make check
+  make install
 fi
-
-make
-make check
-make install
