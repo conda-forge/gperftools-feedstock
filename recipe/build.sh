22a58341a36aa596885f25ad6bd400b83f1cb729
Matthias Diener <matthias.diener@gmail.com>
2023-08-16 16:52:35 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,8 +18,7 @@ else
   make -j${CPU_COUNT}
 
   if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
-    make check
-    cat ./test-suite.log
+    make check || { cat ./test-suite.log; exit 1; }
   fi
 
   make install
